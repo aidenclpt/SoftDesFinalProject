61f9dd9fc348fba860fc9df810014e5c05a178d4
aidenclpt <aiden.carley-clopton@students.olin.edu>
2018-04-13 14:52:01 -0400

    testing gitignore

--- image_load.py
+++ image_load.py
@@ -2,6 +2,15 @@ from PIL import Image
 import os
 import numpy as np
 
+def load_image(file_path):
+
+    im = Image.open(file_path)
+    bands = im.split()
+
+    values = []
+    for band in bands:
+        values.append(band.load())
+
 def balance_levels(red_channel, blue_channel, green_channel):
     red_vector = []
     blue_vector = []
@@ -42,65 +51,67 @@ def balance_levels(red_channel, blue_channel, green_channel):
     return red_pixels, green_pixels, blue_pixels
 
 
-results_folder = 'image_decomp'
+results_folder = 'results'
 
 if not os.path.exists(results_folder):
     os.makedirs(results_folder)
 
 #image_name = input("Enter Image Name") + '.png'
+data_folder = '/home/aiden/Final_Project_Image_Repo/images/'
+def merge_landsat():
+    name_1 = data_folder + 'band_1.png'
+    name_2 = data_folder + 'band_2.png'
+    name_3 = data_folder + 'band_3.png'
+    name_4 = data_folder + 'band_4.png'
+    name_5 = data_folder + 'band_5.png'
+    name_6 = data_folder + 'band_6.png'
+    name_7 = data_folder + 'band_7.png'
 
-name_1 = 'images/band_1.png'
-name_2 = 'images/band_2.png'
-name_3 = 'images/band_3.png'
-name_4 = 'images/band_4.png'
-name_5 = 'images/band_5.png'
-name_6 = 'images/band_6.png'
-name_7 = 'images/band_7.png'
-
-band_1 = Image.open(name_1)
-band_2 = Image.open(name_2)
-band_3 = Image.open(name_3)
-band_4 = Image.open(name_4)
-band_5 = Image.open(name_5)
-band_6 = Image.open(name_6)
-band_7 = Image.open(name_7)
-
-
-blue = band_2.split()[0]
-green = band_3.split()[0]
-red = band_4.split()[0]
-NIR = band_5.split()[0]
-IR1 = band_6.split()[0]
-IR2 = band_7.split()[0]
-
-"""red_values = red.load()
-blue_values = blue.load()
-green_values = green.load()"""
-#red_values, blue_values, green_values = balance_levels(red,blue,green)
-
-NIR_values,IR1_values,IR2_values = balance_levels(NIR,IR1,IR2)
-
-small_im_size = (7000,7000)
-
-#search_image = Image.open(image_name)
-#im_size = search_image.size
+    band_1 = Image.open(name_1)
+    band_2 = Image.open(name_2)
+    band_3 = Image.open(name_3)
+    band_4 = Image.open(name_4)
+    band_5 = Image.open(name_5)
+    band_6 = Image.open(name_6)
+    band_7 = Image.open(name_7)
 
 
-"""color_im = Image.new('RGB', small_im_size)
-pixels = color_im.load()
-for i in range(small_im_size[0]):
-    for j in range(small_im_size[1]):
-        pixels[i,j] = (red_values[i,j], green_values[i,j], blue_values[i,j])
+    blue = band_2.split()[0]
+    green = band_3.split()[0]
+    red = band_4.split()[0]
+    NIR = band_5.split()[0]
+    IR1 = band_6.split()[0]
+    IR2 = band_7.split()[0]
 
-color_im.save('images/test_color.png')"""
+    """red_values = red.load()
+    blue_values = blue.load()
+    green_values = green.load()"""
+    #red_values, blue_values, green_values = balance_levels(red,blue,green)
 
-IR_im = Image.new('RGB', small_im_size)
-pixels = IR_im.load()
-for i in range(small_im_size[0]):
-    for j in range(small_im_size[1]):
-        pixels[i,j] = (NIR_values[i,j], IR1_values[i,j], IR2_values[i,j])
+    NIR_values,IR1_values,IR2_values = balance_levels(NIR,IR1,IR2)
 
-IR_im.save('images/test_IR.png')
+    small_im_size = (7000,7000)
+
+    #search_image = Image.open(image_name)
+    #im_size = search_image.size
+
+
+    color_im = Image.new('RGB', small_im_size)
+    pixels = color_im.load()
+
+    for i in range(small_im_size[0]):
+        for j in range(small_im_size[1]):
+            pixels[i,j] = (red_values[i,j], green_values[i,j], blue_values[i,j])
+
+    color_im.save('images/test_color.png')
+
+    IR_im = Image.new('RGB', small_im_size)
+    pixels = IR_im.load()
+    for i in range(small_im_size[0]):
+        for j in range(small_im_size[1]):
+            pixels[i,j] = (NIR_values[i,j], IR1_values[i,j], IR2_values[i,j])
+
+    IR_im.save('results/test_IR.png')
 
 
 """for i in range(1,im_size(1)//small_im_size(1)):
