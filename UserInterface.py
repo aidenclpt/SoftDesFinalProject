79446b31a3c2b1bc66604ab8197b7e215b9ddd9b
rickyroze <rickyroze@gmail.com>
2018-04-26 22:35:34 -0400

    commiting for grace

--- UserInterface.py
+++ UserInterface.py
@@ -4,16 +4,17 @@ from PIL import Image, ImageTk
 import tkinter.simpledialog
 
 def paint(event):
-    python_green = "#476042"
-    x1, y1 = (event.x - 1), (event.y - 1)
-    x2, y2 = (event.x + 1), (event.y + 1)
-    w.create_oval(x1, y1, x2, y2, fill=python_green)
+    FillColor = "#FF0000"
+    x1, y1 = (event.x - 2), (event.y - 2)
+    x2, y2 = (event.x + 2), (event.y + 2)
+    w.create_oval(x1, y1, x2, y2, fill=FillColor)
     x = event.x
     y = event.y
     coordlist = []
     coordlist.append(x)
     coordlist.append(y)
     return coordlist
+    print(coordlist)
 root = Tk()
 #setting up window
 root.title("Draw Some Points!")
@@ -29,7 +30,7 @@ parentDir = str(File)
 parentDir = parentDir.rsplit('/', 1)[0]
 # find path and subtract image name to get parent direcrtory.  make variable name.
 
-print(coordlist)
+
 
 
 root.mainloop()
