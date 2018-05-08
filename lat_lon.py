96678cbec3106c5aa962e73686bf041c5d864a05
aidenclpt <aiden.carley-clopton@students.olin.edu>
2018-05-08 10:40:57 -0400

    Fixed a bug where vertices of an incomplete polygon wouldn't zoom properly

--- lat_lon.py
+++ lat_lon.py
@@ -130,13 +130,12 @@ class GeoPoly:
         self.shapely_coords = []
         self.point_list = point_list
 
+        for point in point_list:
+            self.kinter_coords.append(point.x)
+            self.kinter_coords.append(point.y)
+            self.shapely_coords.append(point.utm)
+
         if len(self.point_list) >=3:
-            for point in point_list:
-                self.kinter_coords.append(point.x)
-                self.kinter_coords.append(point.y)
-
-                self.shapely_coords.append(point.utm)
-
             self.shapely_coords = tuple(self.shapely_coords)
             self.poly = Polygon(self.shapely_coords)
             self.area = self.poly.area
@@ -146,13 +145,13 @@ class GeoPoly:
         self.area = 0
         self.shapely_coords = []
         self.kinter_coords = []
+
+        for point in self.point_list:
+            self.kinter_coords.append(point.x)
+            self.kinter_coords.append(point.y)
+            self.shapely_coords.append(point.utm)
+
         if len(self.point_list) >=3:
-            for point in self.point_list:
-                self.kinter_coords.append(point.x)
-                self.kinter_coords.append(point.y)
-
-                self.shapely_coords.append(point.utm)
-
             self.shapely_coords = tuple(self.shapely_coords)
             self.poly = Polygon(self.shapely_coords)
             self.area = self.poly.area
@@ -168,6 +167,7 @@ class GeoSegments:
         self.lines = []
         self.length = 0
         self.point_list = point_list
+
         if len(self.point_list) >= 2:
             for i in range(len(self.point_list)-1):
                 line = GeoLine(self.point_list[i], self.point_list[i+1])
@@ -178,11 +178,13 @@ class GeoSegments:
         self.length = 0
         self.lines = []
         self.kinter_coords = []
+        
         if len(self.point_list) >= 2:
             for i in range(len(self.point_list)-1):
                 line = GeoLine(self.point_list[i], self.point_list[i+1])
                 self.lines.append(line)
                 self.length += line.real_length
+
         for point in self.point_list:
             self.kinter_coords.append(point.x)
             self.kinter_coords.append(point.y)
